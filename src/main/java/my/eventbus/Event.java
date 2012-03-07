84c8ccc55090dd5a0b12704b0c2f4e2c8dbc4fbd
David <david.ershag@gmail.com>
2012-03-07 22:51:28 +0100

    Minor javadoc modifications

--- src/main/java/my/eventbus/Event.java
+++ src/main/java/my/eventbus/Event.java
@@ -1,8 +1,7 @@
 package my.eventbus;
 
 /**
- * An event that can be fired and handled
+ * An event that can be fired and handled by a {@link my.eventbus.Handler}.
  */
 public interface Event {
-
 }
--- src/main/java/my/eventbus/HandlerRegistration.java
+++ src/main/java/my/eventbus/HandlerRegistration.java
@@ -2,8 +2,8 @@ package my.eventbus;
 
 /**
  * A reference returned by {@link my.eventbus.EventBus#addHandler(Handler)} and
- * {@link my.eventbus.EventBus#addHandlerToSource(Handler, Object)} used to
- * unregister the handler
+ * {@link my.eventbus.EventBus#addHandlerToSource(Handler, Object)} that can be
+ * used to unregister the handler.
  */
 public interface HandlerRegistration {
 
